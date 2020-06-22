fce7e0ecca71b11ca3f297042e4e0d48920f6ee7
YudaiTamura <y-tam@live.jp>
2020-06-22 22:09:24 +0900

    useStateを使ったカウント機能を実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is a template for React app.</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const incrementLoop = () => setCount((previousCount) => (previousCount >= 10 ? 0 : previousCount + 1));
+  const decrementLoop = () => setCount((previousCount) => (previousCount <= -10 ? 0 : previousCount - 1));
+
+  const reset = () => setCount(0);
+
+  return (
+    <>
+      <div>{count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={incrementLoop}>incrementLoop</button>
+        <button onClick={decrementLoop}>decrementLoop</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+      </div>
+    </>
+  );
 };
 
 export default App;

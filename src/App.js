3b9a95425c4ea21e2b47a558ad94aa1024d8041c
YudaiTamura <y-tam@live.jp>
2020-06-23 21:45:40 +0900

    複数の状態を1つのオブジェクトに統一する

--- src/App.js
+++ src/App.js
@@ -11,13 +11,8 @@ const App = (props) => {
 
   const reset = () => setCount(0);
 
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-
-  const resetInput = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
   return (
     <>
@@ -37,10 +32,10 @@ const App = (props) => {
       <p>
         現在の{name}は、{price}円です。
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={resetInput}>reset</button>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
+      <button onClick={() => setState(props)}>reset</button>
+      <input value={name} onChange={(e) => setState({ ...state, name: e.target.value })} />
     </>
   );
 };

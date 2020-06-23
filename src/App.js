1d6509580fcc0bb64f25fc3d92e22343dcb93db0
YudaiTamura <y-tam@live.jp>
2020-06-23 21:09:18 +0900

    複数の状態を管理するように

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const App = () => {
+const App = (props) => {
   const [count, setCount] = useState(0);
 
   const increment = () => setCount(count + 1);
@@ -10,6 +10,14 @@ const App = () => {
   const decrementLoop = () => setCount((previousCount) => (previousCount <= -10 ? 0 : previousCount - 1));
 
   const reset = () => setCount(0);
+
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const resetInput = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
 
   return (
     <>
@@ -25,8 +33,21 @@ const App = () => {
       <div>
         <button onClick={reset}>reset</button>
       </div>
+      <hr />
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={resetInput}>reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "サンプル",
+  price: 1000,
 };
 
 export default App;
